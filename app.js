083ee67da7c996324dd3971f923042a1ed11c4e2
henrilics <henrydeguzman.java73@gmail.com>
2020-11-02 14:42:44 +0800

    Working with Event Arguments

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      addNum: 5
     };
   },
   methods: {
        add(isPlus) {            
-            isPlus ? this.counter++ : this.counter--;
+            isPlus ? this.counter += this.addNum  : this.counter -= this.addNum;
        }
   }
 });
