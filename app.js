53ecd0d709d41cc4153732fa9b62918d92c1802f
henrilics <henrydeguzman.java73@gmail.com>
2020-11-02 20:36:46 +0800

    Introducing Computed Properties

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
 const app = Vue.createApp({
-  data() {
-    return {
-         counter: 10,
-         addNum: 5,
-         name: '',
-         confirmedName: ''
-    };
-  },
-  methods: {
-       confirmInput() {
-            this.confirmedName = this.name
-       },
-       submitForm () {            
-            alert('Submittted')
-       },
-       setName (event, lastName) {            
-            this.name = event.target.value + ' ' + lastName
-       },
-       add(isPlus) {            
-            isPlus ? this.counter += this.addNum  : this.counter -= this.addNum;
-       }
-  }
+	data() {
+		return {
+			counter: 10,
+			addNum: 5,
+			name: "",
+			confirmedName: "",
+		};
+	},
+	computed: {
+		fullname() {
+			console.log("Running again...");
+			if (this.name === "") {
+				return "";
+               }
+               return this.name + ' ' + 'De Guzman'
+		},
+	},
+	methods: {
+		outputFullName() {
+			console.log("Running again...");
+			if (this.name === "") {
+				return "";
+			}
+		},
+		resetInput() {
+			this.name = "";
+		},
+		confirmInput() {
+			this.confirmedName = this.name;
+		},
+		setName(event, lastName) {
+			this.name = event.target.value + " " + lastName;
+		},
+		add(isPlus) {
+			isPlus ? (this.counter += this.addNum) : (this.counter -= this.addNum);
+		},
+	},
 });
 
-app.mount('#events');
+app.mount("#events");
