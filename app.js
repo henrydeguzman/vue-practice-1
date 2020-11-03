9853b1fccc2e8f7330cad296ada84fc5a5ed5df5
henrilics <henrydeguzman.java73@gmail.com>
2020-11-03 10:04:10 +0800

    updates

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const app = Vue.createApp({
-  data() {
-    return { goals: [] };
-  },
+	data() {
+		return {
+               enteredValue: '',
+               goals: []
+          };
+     },
+     computed: {          
+     },
+	methods: {
+          remove(idx) {
+               console.log(idx)
+               this.goals.splice(idx, 1);
+          },
+          addGoal () {
+               this.goals.push({
+                    name: this.enteredValue
+               })
+          },
+          sm () {
+               console.log('sm')
+          }
+     }
 });
 
-app.mount('#user-goals');
+app.mount("#user-goals");
